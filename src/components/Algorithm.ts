2685d509ef5c28e54a26df2cb0be73ee1016aee8
ruthrootz <ruthrootz@users.noreply.github.com>
2019-12-26 18:11:15 -0500

    implemented Algorithm component

--- src/components/Algorithm.ts
+++ src/components/Algorithm.ts
@@ -1,8 +1,17 @@
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
 import WithRender from './algorithm.html';
 
 @WithRender
 @Component
 export default class Algorithm extends Vue {
-
+    @Prop()
+    private algName: string;
+    @Prop()
+    private steps: string;
+    private stepsLink: string = '';
+    private imageSource: string = name + '.png';
+    private created(): void {
+        this.stepsLink = 'https://alg.cubing.net/?view=playback&type=alg&title=OLL%20Case%2027%20Sune&setup=._x2_.&alg='
+        + this.steps.replace('\'', '-');
+    }
 }
--- src/components/Algorithms.ts
+++ src/components/Algorithms.ts
@@ -1,7 +1,13 @@
 import { Component, Vue, Prop } from 'vue-property-decorator';
 import WithRender from './algorithms.html';
+import Algorithm from './Algorithm';
 
 @WithRender
-@Component
+@Component({
+    components: {
+        Algorithm,
+    },
+})
 export default class Algorithms extends Vue {
+
 }
