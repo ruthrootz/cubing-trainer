d95535ffd0a6ce897af08309e6237fc1eeb930d4
ruthrootz <ruthrootz@users.noreply.github.com>
2020-01-25 18:40:27 -0500

    trying to get keyup.space to track time

--- src/components/TimerComponent.ts
+++ src/components/TimerComponent.ts
@@ -6,6 +6,16 @@ require('../styles/timer-component.css');
 @Component
 export default class TimerComponent extends Vue {
 
-    
+    private time: number = 0;
+
+    private mounted(): void {
+        window.addEventListener("keypress", e => {
+            console.log(String.fromCharCode(e.keyCode));
+        });
+    }
+
+    private timerTrigger(): void {
+        console.log('test');
+    }
 
 }
