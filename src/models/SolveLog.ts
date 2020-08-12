48111354ceb70e42979c24132d0f8f2c40489048
Ruth Oldja <roldja@foundationsoft.com>
2020-08-12 14:29:49 -0400

    added +2 button

--- src/models/SolveLog.ts
+++ src/models/SolveLog.ts
@@ -2,6 +2,7 @@ export class SolveLog {
     public id: number;
     public time: number;
     public dnf: boolean | string;
+    public showPlusTwo: boolean = false;
     // public sessionId: number;
     // public userId: number | null;
 
@@ -9,6 +10,7 @@ export class SolveLog {
         this.id = Number(solve.id);
         this.time = Number(solve.time);
         this.dnf = Boolean(solve.dnf);
+        this.showPlusTwo = Boolean(solve.showPlusTwo);
         // this.sessionId = Number(solve.sessionId);
         // this.userId = Number(solve.userId);
     }
