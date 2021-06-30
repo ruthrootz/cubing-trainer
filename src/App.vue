d7e40d856e519f8578bb5d3e530b9e57f30e44dd
Ruth Oldja <rutholdja@icloud.com>
2021-06-30 13:41:09 +0000

    increased tab to 4

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,56 @@
 <template>
-  <div id="app">
-    <link href="https://fonts.googleapis.com/css?family=Space+Mono&display=swap" rel="stylesheet">
-    <h1 id="nav">cubing trainer <span id="info"><i class="fas fa-info-circle"></i></span></h1>
-    <b-popover target="info" triggers="hover" placement="bottom">
-      <p class="popover-text">The scrambles are for 3x3 cubes.</p>
-      <p class="popover-text">Use SPACEBAR to start/stop timer. Use ESC to clear the timer.</p>
-      <p class="popover-text">Click on the X in the DNF cell to toggle DNF status.</p>
-      <p class="popover-text">Hover (or touch on mobile) on time cell to +2.</p>
-      <p class="popover-text">created by: <a href="https://rutholdja.netlify.app/">Ruth O.</a></p>
-    </b-popover>
-    <!-- <div id="nav">
-      <router-link to="/">Timer</router-link> |
-      <router-link to="/algorithms">Algorithms</router-link>
-    </div> -->
-    <router-view/>
-    <notifications group="notifications" />
-  </div>
+    <div id="app">
+        <link href="https://fonts.googleapis.com/css?family=Space+Mono&display=swap" rel="stylesheet">
+        <h1 id="nav">cubing trainer <span id="info"><i class="fas fa-info-circle"></i></span></h1>
+        <b-popover target="info" triggers="hover" placement="bottom">
+            <p class="popover-text">The scrambles are for 3x3 cubes.</p>
+            <p class="popover-text">Use SPACEBAR to start/stop timer. Use ESC to clear the timer.</p>
+            <p class="popover-text">Click on the X in the DNF cell to toggle DNF status.</p>
+            <p class="popover-text">Hover (or touch on mobile) on time cell to +2.</p>
+            <p class="popover-text">created by: <a href="https://rutholdja.netlify.app/">Ruth O.</a></p>
+        </b-popover>
+        <!-- <div id="nav">
+            <router-link to="/">Timer</router-link> |
+            <router-link to="/algorithms">Algorithms</router-link>
+        </div> -->
+        <router-view/>
+        <notifications group="notifications" />
+    </div>
 </template>
 
 <style>
 * {
-  background-color: #2c3e50;
-  user-select: none;
+    background-color: #2c3e50;
+    user-select: none;
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #ffffff;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #ffffff;
 }
 
 #nav {
-  padding: 10px;
-  color: rgb(15, 220, 121);
+    padding: 10px;
+    color: rgb(15, 220, 121);
 }
 
 #nav a {
-  font-weight: bold;
-  color: #ffffff;
+    font-weight: bold;
+    color: #ffffff;
 }
 
 #nav a.router-link-exact-active {
-  color: #fffb05;
+    color: #fffb05;
 }
 
 #info {
-  font-size: 0.5em;
+    font-size: 0.5em;
 }
 
 .popover-text {
-  color: #ffffff;
+    color: #ffffff;
 }
 </style>
